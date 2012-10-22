f83febecd430db28611f746e85a26a0a19de5f8d
Avadhesh <avadheshnatani@gmail.com>
2012-10-23 00:46:29 +0530

    passsword file
    hello

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'honeycreations');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root#123');
 
 /** MySQL hostname */
 define('DB_HOST', '127.0.0.1');
